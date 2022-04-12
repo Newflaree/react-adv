8fe163cb62639304c02b473909ef1fb554ed4ab6
Camilo López <camiloj.lopez79@gmail.com>
2022-04-12 13:24:09 -0400

    Crear archivo de rutas independiente

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,13 +5,8 @@ import {
 	NavLink,
 	Navigate
 } from 'react-router-dom';
-// Components
-import { 
-	LazyPage1, 
-	LazyPage2, 
-	LazyPage3 
-} from '../01-lazyload/pages';
 import logo from '../logo.svg';
+import { routes }from './routes';
 
 export const Navigation = () => {
   return(
@@ -21,39 +16,33 @@ export const Navigation = () => {
 					<img src={ logo } alt='React Logo' />
 
 					<ul>
-						<li>
-							<NavLink 
-								to='/lazy1' 
-								className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-							>
-								Lazy 1
-							</NavLink>
-						</li>
-						<li>
-							<NavLink 
-								to='/lazy2' 
-								className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-							>
-								Lazy 2
-							</NavLink>
-						</li>
-						<li>
-							<NavLink 
-								to='/lazy3' 
-								className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-							>
-								Lazy 3
-							</NavLink>
-						</li>
+						{
+							routes.map( ({ to, name }) => (
+								<li key={ to }>
+									<NavLink 
+										to={ to }
+										className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+									>
+										{ name }
+									</NavLink>
+								</li>
+							))
+						}
 					</ul>
 				</nav>
 
 				<Routes>
-					<Route path='lazy1' element={ <LazyPage1 /> } />
-					<Route path='lazy2' element={ <LazyPage2 /> } />
-					<Route path='lazy3' element={ <LazyPage3 /> } />
+					{
+						routes.map( ({ to, path, Component }) => (
+							<Route
+								key={ to } 
+								path={ path }
+								element={ <Component /> } 
+							/>
+						))
+					}
 
-					<Route path='/*' element={ <Navigate to='/lazy1' replace /> } />
+					<Route path='/*' element={ <Navigate to={ routes[0].to } replace /> } />
 				</Routes>
 			</div>
 		</BrowserRouter>
