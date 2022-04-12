9f97fb636c84a2b9a2fdcf7421b2b9c57d6337b2
Camilo López <camiloj.lopez79@gmail.com>
2022-04-12 13:50:53 -0400

    Lazyload y Suspense

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { 
 	BrowserRouter,
 	Routes, 
@@ -10,41 +11,44 @@ import { routes }from './routes';
 
 export const Navigation = () => {
   return(
-		<BrowserRouter>
-			<div className='main-layout'>
-				<nav>
-					<img src={ logo } alt='React Logo' />
+		<Suspense fallback={ <span>Loading...</span> }>
 
-					<ul>
+			<BrowserRouter>
+				<div className='main-layout'>
+					<nav>
+						<img src={ logo } alt='React Logo' />
+
+						<ul>
+							{
+								routes.map( ({ to, name }) => (
+									<li key={ to }>
+										<NavLink 
+											to={ to }
+											className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+										>
+											{ name }
+										</NavLink>
+									</li>
+								))
+							}
+						</ul>
+					</nav>
+
+					<Routes>
 						{
-							routes.map( ({ to, name }) => (
-								<li key={ to }>
-									<NavLink 
-										to={ to }
-										className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-									>
-										{ name }
-									</NavLink>
-								</li>
+							routes.map( ({ to, path, Component }) => (
+								<Route
+									key={ to } 
+									path={ path }
+									element={ <Component /> } 
+								/>
 							))
 						}
-					</ul>
-				</nav>
 
-				<Routes>
-					{
-						routes.map( ({ to, path, Component }) => (
-							<Route
-								key={ to } 
-								path={ path }
-								element={ <Component /> } 
-							/>
-						))
-					}
-
-					<Route path='/*' element={ <Navigate to={ routes[0].to } replace /> } />
-				</Routes>
-			</div>
-		</BrowserRouter>
+						<Route path='/*' element={ <Navigate to={ routes[0].to } replace /> } />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</Suspense>
   );
 }
