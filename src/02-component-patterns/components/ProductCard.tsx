3f5dc3957331c3de06ce1c6d35a64955806ed2af
Camilo López <camiloj.lopez79@gmail.com>
2022-04-12 22:28:36 -0400

    Compound Component Pattern - Primeros pasos

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -12,19 +12,34 @@ interface Product {
   img?: string;
 }
 
-export const ProductCard = ( { product }: Props ) => {
-  const { counter, increaseBy } = useProduct();
-
-  return (
-    <div className={ styles.productCard }>
+export const ProductImage = ({ img = '' }) => {
+  return(
       <img 
         className={ styles.productImg }
-        src={ product.img ? product.img : noImage }
-        alt='Coffee Mug' 
+        src={ img ? img : noImage }
+        alt='Product image' 
       />
+  );
+}
 
-      <span className={ styles.productDescription }>{ product.title }</span>
+// Deferente forma de pitar el argumento de un función
+export const ProductTitle = ({ title }: { title: string }) => {
+  return(
+      <span 
+        className={ styles.productDescription }
+      >
+        { title }
+      </span>
+  );
+}
 
+interface ProductButtonsProps {
+  counter: number;
+  increaseBy: (value: number) => void;
+}
+
+export const ProductButtons = ({ counter, increaseBy }: ProductButtonsProps ) => {
+  return(
       <div className={ styles.buttonsContainer }>
         <button
           className={ styles.buttonMinus }
@@ -39,11 +54,25 @@ export const ProductCard = ( { product }: Props ) => {
         </div>
         <button
           className={ styles.buttonAdd }
-          onClick={ () => increaseBy( 1 ) }
+          onClick={ () => increaseBy( 1) }
         > 
           + 
         </button>
       </div>
+
+  );
+}
+
+export const ProductCard = ( { product }: Props ) => {
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <div className={ styles.productCard }>
+      <ProductImage img={ product.img } />
+
+      <ProductTitle title={ product.title } />
+
+      <ProductButtons counter={ counter } increaseBy={ increaseBy }  />
     </div>
-  )
+  );
 }
