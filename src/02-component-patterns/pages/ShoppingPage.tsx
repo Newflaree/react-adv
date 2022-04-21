6f7c56db4a33bdbcf27c4a3ba9b787a9fe381b43
Camilo López <camiloj.lopez79@gmail.com>
2022-04-21 14:53:50 -0400

    Se prepara proyecto para continuar la sección

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -6,67 +6,30 @@ import {
   ProductTitle 
 } from "../components"
 // Custom Hooks
-import { useShoppingCart } from "../hooks/useShoppingCart";
 // Data
 import { products } from "../data/data";
 // Styles
 import '../styles/custom-styles.css';
 
+const product = products[0];
+
 export const ShoppingPage = () => {
-	const { shoppingCart, onProductCountChange } = useShoppingCart()
+
 
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
 
-      <div style={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-      }}>
-        {
-          products.map( product => (
-            <ProductCard 
-              key={ product.id }
-              product={ product }
-              className='bg-dark text-white'
-							onChange={ onProductCountChange }
-							value={ shoppingCart[product.id]?.count || 0 }
-            >
-              <ProductImage className='custom-image' />
-              <ProductTitle className='text-white text-bold' />
-              <ProductButtons className="custom-buttons" />
-            </ProductCard>
-          ))
-        }
-      </div>
-
-      <div className="shopping-cart">
-				{
-					Object.entries( shoppingCart ).map( ([ key, product ]) => (
-        		<ProductCard 
-							key={ key }
-          		product={ product }
-          		className='bg-dark'
-          		style={{ 
-            		width: '100px'
-          		}}
-							onChange={ onProductCountChange }
-							value={ product.count }
-        		>
-          		<ProductImage className='custom-image' />
-							<ProductButtons 
-								className="custom-buttons" 
-								style={{
-									display: 'flex',
-									justifyContent: 'center'
-								}}
-							/>
-        		</ProductCard>
-					))
-				}
-      </div>
+      <ProductCard 
+        key={ product.id }
+        product={ product }
+        className='bg-dark text-white'
+      >
+        <ProductImage className='custom-image' />
+        <ProductTitle className='text-white text-bold' />
+        <ProductButtons className="custom-buttons" />
+      </ProductCard>
     </div>
   )
 }
